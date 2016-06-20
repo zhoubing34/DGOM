881a4109aa79724bcd831149181bbad238f2723e
li12242 <li12242@tju.edu.cn>
2016-06-20 11:12:52 +0800

    add PhysDomain

--- library/PhysDomain/PhysDomain.c
+++ library/PhysDomain/PhysDomain.c
@@ -1,5 +1,32 @@
-//
-// Created by li12242 on 16/6/20.
-//
+/**
+ * @file
+ * Set physical variables
+ *
+ * @brief
+ *
+ * @author
+ * li12242, Tianjin University, li12242@tju.edu.cn
+ */
 
 #include "PhysDomain.h"
+
+PhysDomain2d* GetPhysDomain2d(MultiReg2d *mesh, int Nfields){
+    PhysDomain2d *phys = (PhysDomain2d *) calloc(1, sizeof(PhysDomain2d));
+
+    /* number of variables */
+    phys->Nfields = Nfields;
+
+    /* mesh */
+    phys->mesh = mesh;
+    /* data array */
+    phys->f_Q    = (float *) calloc(mesh->K*mesh->StdElement->Np*Nfields, sizeof(float));
+    phys->f_rhsQ = (float *) calloc(mesh->K*mesh->StdElement->Np*Nfields, sizeof(float));
+    phys->f_resQ = (float *) calloc(mesh->K*mesh->StdElement->Np*Nfields, sizeof(float));
+
+    /* MPI send/recv buffer */
+    phys->f_inQ  = (float *) calloc(mesh->parNtotalout*Nfields, sizeof(float));
+    phys->f_outQ = (float *) calloc(mesh->parNtotalout*Nfields, sizeof(float));
+
+    phys->surfinfo = (float *) calloc(mesh->K*)
+
+}
--- library/PhysDomain/PhysDomain.h
+++ library/PhysDomain/PhysDomain.h
@@ -5,4 +5,20 @@
 #ifndef DGOM_PHYSDOMAIN_H
 #define DGOM_PHYSDOMAIN_H
 
+#include "MultiRegions/MultiRegions.h"
+
+typedef struct PhysDomain2d{
+    /** number of variable fields */
+    int Nfields;
+    /** mesh */
+    MultiReg2d* mesh;
+    /** surf infomation */
+    float *surfinfo;
+    /** variable array */
+    float *f_Q, *f_rhsQ, *f_resQ;
+
+    /** send/recv data */
+    float *f_inQ, *f_outQ;
+}PhysDomain2d;
+
 #endif //DGOM_PHYSDOMAIN_H
