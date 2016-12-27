8c0d6c9844c7603514cd3606c1137fefbf0a3360
li12242 <li12242@tju.edu.cn>
2016-12-27 17:05:51 +0800

    delete unused folder

--- library/UnitTest/LibUtilities/LibUtiltiesTest.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBUTILITIES_TEST_H
-#define LIBUTILITIES_TEST_H
-
-#include "LibUtilities/LibUtilities.h"
-#include "LibUtilities/UnstructMesh.h"
-#include "LibUtilities/GenUniformMesh.h"
-#include <stdio.h>
-
-#endif
--- library/UnitTest/PhysDomain/PhysDomainTest.h
+++
@@ -1,6 +0,0 @@
-#ifndef PHYSDOMAIN_TEST_H
-#define PHYSDOMAIN_TEST_H
-
-#include "PhysDomain/PhysDomain.h"
-
-#endif
