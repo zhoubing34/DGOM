eb341fe381fd3d361dd8408176034ab90d8131c4
li12242 <li12242@tju.edu.cn>
2017-03-13 16:39:00 +0800

    fix phys_init

--- library/PhysField/dg_phys_init.c
+++ library/PhysField/dg_phys_init.c
@@ -8,6 +8,10 @@
  * @brief initialize the physical fields with initial condition file.
  * @param phys physical structure
  * @param casename name of computational case
+ * @note
+ * The EToV in phys->grid structure is different from the original EToV
+ * variable. Therefore, a new EToV is read from the input element file
+ * and assigned in the function.
  */
 void dg_phys_init_file2d(dg_phys *phys, char *casename){
     char init_filename[MAX_NAME_LENGTH];
@@ -31,27 +35,53 @@ void dg_phys_init_file2d(dg_phys *phys, char *casename){
     /* read vertex initial values */
     register int n,fld,k;
     int tmp;
-    dg_real **intval = matrix_real_create(Nfield, Nvert);
+    dg_real **init_value = matrix_real_create(Nfield, Nvert);
     for(n=0;n<Nvert;n++){
         fscanf(fp, "%d", &tmp);
         for(fld=0;fld<Nfield;fld++){
-            fscanf(fp, "%lf", intval[fld]+n);
+            fscanf(fp, "%lf", init_value[fld]+n);
         }
     }
 
+    /* read element file */
+    char element_file[MAX_NAME_LENGTH];
+    strcpy(element_file, casename);
+    strcat(element_file, ".ele");
+    if( (fp = fopen(element_file, "r")) == NULL ){
+        fprintf(stderr, "%s (%d)\n"
+                        "Unable to open element file %s.\n",
+                __FUNCTION__,__LINE__,element_file);
+    }
+    int Nv, K, temp;
+    fscanf(fp, "%d %d %d\n", &K, &Nv, &temp);
+    // check element vertex
+    if(dg_cell_Nv(phys->cell) !=  Nv){
+        fprintf(stderr, "%s (%d)\n"
+                "The input element type is not correct!\n", __FILE__, __LINE__);
+        exit(-1);
+    }
+    int **EToV = matrix_int_create(K, Nv);
+    for(k=0;k<K;k++){
+        fscanf(fp, "%d", &temp); //read index
+        for(n=0;n<Nv;n++){
+            fscanf(fp, "%d", EToV[0]+k*Nv+n);
+            EToV[k][n] -= 1; // change index start from 0 (C style)
+        }
+        fscanf(fp, "%d", &temp); //read region id
+    }
+    fclose(fp);
+
+
     /* assign to node fields */
     dg_cell *cell = phys->cell;
-    const int Nv = dg_cell_Nv(cell);
-    const int K = dg_grid_K(phys->grid);
     const int Np = dg_cell_Np(cell);
 
-    int **EToV = phys->grid->EToV;
     dg_real floc_v[Nv], floc[Np];
     dg_real *f_Q = phys->f_Q;
     for(k=0;k<K;k++){
         for(fld=0;fld<Nfield;fld++){
             for(n=0;n<Nv;n++){ // vertex initial values
-                floc_v[n] = intval[fld][ EToV[k][n] ];
+                floc_v[n] = init_value[fld][ EToV[k][n] ];
             }
             /* map from vertex to nodes */
             dg_cell_proj_vert2node(cell, floc_v, floc);
@@ -60,6 +90,8 @@ void dg_phys_init_file2d(dg_phys *phys, char *casename){
             }
         }
     }
-    matrix_real_free(intval);
+
+    matrix_real_free(init_value);
+    matrix_int_free(EToV);
     return;
 }
